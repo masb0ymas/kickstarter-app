e926f3d6d8e6a8c66441798ff9de8d729f8eb1b5
masb0ymas <n.fajri@outlook.com>
2024-06-29 12:41:54 +0700

    fix: update utils string

--- src/core/utils/string.ts
+++ src/core/utils/string.ts
@@ -1,19 +1,39 @@
 /**
  *
- * @param address
+ * @param value
  * @returns
  */
-export function hiddenAddress(address: string) {
-  const address_length = address.length;
-  const hide_length = address_length - 25;
+export function hiddenString(
+  value: string,
+  start_hide: number,
+  position: number
+) {
+  const value_length = value.length;
+  const hide_length = value_length - start_hide;
 
-  const digit_position = 15;
+  const digit_position = position;
 
-  const start_value = address.slice(0, hide_length);
-  const end_value = address.slice(hide_length + digit_position);
+  const start_value = value.slice(0, hide_length);
+  const end_value = value.slice(hide_length + digit_position);
 
   const repeat = "*".repeat(digit_position - 5);
   const result = `${start_value}${repeat}${end_value}`;
+
+  return result;
+}
+
+/**
+ *
+ * @param address
+ * @param start_hide
+ * @param position
+ * @returns
+ */
+export function hiddenAddress(address: string, start_hide?: number, position?: number) {
+  const new_start_hide = start_hide || 25
+  const new_position = position || 15
+
+  const result = hiddenString(address, new_start_hide, new_position);
 
   return result;
 }
