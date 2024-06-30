db08713cd7226946edb7774d7990157f9aceda52
masb0ymas <n.fajri@outlook.com>
2024-06-30 17:28:51 +0700

    fix: node campaign

--- src/solidity/node/campaign.js
+++ src/solidity/node/campaign.js
@@ -1,8 +1,5 @@
+import web3 from "~/config/web3";
 const Campaign = require("../build/Campaign.json");
-const { Web3 } = require("web3");
-
-const provider = new Web3.providers.HttpProvider(process.env.YOUR_INFURA_URL);
-const web3 = new Web3(provider);
 
 const campaign = (address) => {
   return new web3.eth.Contract(Campaign.abi, address);
--- src/solidity/node/factory.js
+++ src/solidity/node/factory.js
@@ -1,8 +1,5 @@
+import web3 from "~/config/web3";
 const CampaignFactory = require("../build/CampaignFactory.json");
-const { Web3 } = require("web3");
-
-const provider = new Web3.providers.HttpProvider(process.env.YOUR_INFURA_URL);
-const web3 = new Web3(provider);
 
 const campaignFactory = (address) => {
   return new web3.eth.Contract(CampaignFactory.abi, address);
