ff23ee66f02bd25884c3ac8317056216be1c0623
masb0ymas <n.fajri@outlook.com>
2024-07-04 10:09:40 +0700

    fix: deploy contract

--- src/solidity/node/deploy.js
+++ src/solidity/node/deploy.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const { Web3 } = require("web3");
 
-const compiledFactory = require("../build/CampaignFactory.json");
+const contractFactory = require("../build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
   process.env.YOUR_MNEMONIC,
@@ -17,11 +17,19 @@ const deploy = async () => {
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(compiledFactory.abi)
-    .deploy({ data: compiledFactory.evm.bytecode.object })
-    .send({ gas: "1400000", from: accounts[0] });
+  try {
+    const result = await new web3.eth.Contract(contractFactory.abi)
+      .deploy({ data: contractFactory.evm.bytecode.object })
+      .send({ gas: "1400000", from: accounts[0] });
 
-  console.log("Contract deployed to", result.options.address);
+    console.log(
+      "Contract 'Campaign Factory' deployed to",
+      result.options.address
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
   provider.engine.stop();
 };
 
